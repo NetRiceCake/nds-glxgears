a003645c8fad6c6508c3a369595bf8fe4700a39f
NetRiceCake <afterlib@gmail.com>
2021-08-15 20:29:28 +0900

    Upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# glxgears
+Ported glxgears for run on Nintendo DS.
+
+![image](https://cdn.discordapp.com/attachments/876173640226443267/876173788868399154/ezgif-2-01bd800b0379.webp "running on nintendo ds lite")
+
+### Build
+1.You need to install the devkitpro(nds-dev)<br/>
+2.Build: ```make```
---
+++ source/main.cpp
@@ -0,0 +1,237 @@
+#include <nds.h>
+#include <filesystem.h>
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+#ifndef TIMER_SPEED
+#define TIMER_SPEED (BUS_CLOCK/1024)
+#endif
+
+#ifndef M_PI
+#define M_PI 3.14159265
+#endif
+
+float view_rotx = 20.0, view_roty = 30.0, view_rotz = 0.0;
+int gear1, gear2, gear3;
+float angle = 0.0;
+
+void gear(float inner_radius, float outer_radius, float width, int teeth, float tooth_depth)
+{
+   int i;
+   float r0, r1, r2;
+   float angle, da;
+   float u, v, len;
+
+   r0 = inner_radius;
+   r1 = outer_radius - tooth_depth / 2.0;
+   r2 = outer_radius + tooth_depth / 2.0;
+
+   da = 2.0 * M_PI / teeth / 4.0;
+
+   glNormal3f(0.0, 0.0, 1.0);
+
+   /* draw front face */
+   glBegin(GL_QUAD_STRIP);
+   for (i = 0; i <= teeth; i++) {
+      angle = i * 2.0 * M_PI / teeth;
+      glVertex3f(r0 * cos(angle), r0 * sin(angle), width * 0.5);
+      glVertex3f(r1 * cos(angle), r1 * sin(angle), width * 0.5);
+      if (i < teeth) {
+	 glVertex3f(r0 * cos(angle), r0 * sin(angle), width * 0.5);
+	 glVertex3f(r1 * cos(angle + 3 * da), r1 * sin(angle + 3 * da),
+		    width * 0.5);
+      }
+   }
+   glEnd();
+
+   /* draw front sides of teeth */
+   glBegin(GL_QUADS);
+   da = 2.0 * M_PI / teeth / 4.0;
+   for (i = 0; i < teeth; i++) {
+      angle = i * 2.0 * M_PI / teeth;
+
+      glVertex3f(r1 * cos(angle), r1 * sin(angle), width * 0.5);
+      glVertex3f(r2 * cos(angle + da), r2 * sin(angle + da), width * 0.5);
+      glVertex3f(r2 * cos(angle + 2 * da), r2 * sin(angle + 2 * da),
+		 width * 0.5);
+      glVertex3f(r1 * cos(angle + 3 * da), r1 * sin(angle + 3 * da),
+		 width * 0.5);
+   }
+   glEnd();
+
+   glNormal3f(0.0, 0.0, -1.0);
+
+   /* draw back face */
+   glBegin(GL_QUAD_STRIP);
+   for (i = 0; i <= teeth; i++) {
+      angle = i * 2.0 * M_PI / teeth;
+      glVertex3f(r1 * cos(angle), r1 * sin(angle), -width * 0.5);
+      glVertex3f(r0 * cos(angle), r0 * sin(angle), -width * 0.5);
+      if (i < teeth) {
+	 glVertex3f(r1 * cos(angle + 3 * da), r1 * sin(angle + 3 * da),
+		    -width * 0.5);
+	 glVertex3f(r0 * cos(angle), r0 * sin(angle), -width * 0.5);
+      }
+   }
+   glEnd();
+
+   /* draw back sides of teeth */
+   glBegin(GL_QUADS);
+   da = 2.0 * M_PI / teeth / 4.0;
+   for (i = 0; i < teeth; i++) {
+      angle = i * 2.0 * M_PI / teeth;
+
+      glVertex3f(r1 * cos(angle + 3 * da), r1 * sin(angle + 3 * da),
+		 -width * 0.5);
+      glVertex3f(r2 * cos(angle + 2 * da), r2 * sin(angle + 2 * da),
+		 -width * 0.5);
+      glVertex3f(r2 * cos(angle + da), r2 * sin(angle + da), -width * 0.5);
+      glVertex3f(r1 * cos(angle), r1 * sin(angle), -width * 0.5);
+   }
+   glEnd();
+
+   /* draw outward faces of teeth */
+   glBegin(GL_QUAD_STRIP);
+   for (i = 0; i < teeth; i++) {
+      angle = i * 2.0 * M_PI / teeth;
+
+      glVertex3f(r1 * cos(angle), r1 * sin(angle), width * 0.5);
+      glVertex3f(r1 * cos(angle), r1 * sin(angle), -width * 0.5);
+      u = r2 * cos(angle + da) - r1 * cos(angle);
+      v = r2 * sin(angle + da) - r1 * sin(angle);
+      len = sqrt(u * u + v * v);
+      u /= len;
+      v /= len;
+      glNormal3f(v, -u, 0.0);
+      glVertex3f(r2 * cos(angle + da), r2 * sin(angle + da), width * 0.5);
+      glVertex3f(r2 * cos(angle + da), r2 * sin(angle + da), -width * 0.5);
+      glNormal3f(cos(angle), sin(angle), 0.0);
+      glVertex3f(r2 * cos(angle + 2 * da), r2 * sin(angle + 2 * da),
+		 width * 0.5);
+      glVertex3f(r2 * cos(angle + 2 * da), r2 * sin(angle + 2 * da),
+		 -width * 0.5);
+      u = r1 * cos(angle + 3 * da) - r2 * cos(angle + 2 * da);
+      v = r1 * sin(angle + 3 * da) - r2 * sin(angle + 2 * da);
+      glNormal3f(v, -u, 0.0);
+      glVertex3f(r1 * cos(angle + 3 * da), r1 * sin(angle + 3 * da),
+		 width * 0.5);
+      glVertex3f(r1 * cos(angle + 3 * da), r1 * sin(angle + 3 * da),
+		 -width * 0.5);
+      glNormal3f(cos(angle), sin(angle), 0.0);
+   }
+
+   glVertex3f(r1 * cos(0), r1 * sin(0), width * 0.5);
+   glVertex3f(r1 * cos(0), r1 * sin(0), -width * 0.5);
+
+   glEnd();
+
+   /* draw inside radius cylinder */
+   glBegin(GL_QUAD_STRIP);
+   for (i = 0; i <= teeth; i++) {
+      angle = i * 2.0 * M_PI / teeth;
+      glNormal3f(-cos(angle), -sin(angle), 0.0);
+      glVertex3f(r0 * cos(angle), r0 * sin(angle), -width * 0.5);
+      glVertex3f(r0 * cos(angle), r0 * sin(angle), width * 0.5);
+   }
+   glEnd();
+}
+
+void draw(void)
+{
+
+   glPushMatrix();
+   glRotatef(view_rotx, 1.0, 0.0, 0.0);
+   glRotatef(view_roty, 0.0, 1.0, 0.0);
+   glRotatef(view_rotz, 0.0, 0.0, 1.0);
+
+   glPushMatrix();
+   glTranslatef(-3.0, -2.0, 0.0);
+   glRotatef(angle, 0.0, 0.0, 1.0);
+	glMaterialf(GL_AMBIENT_AND_DIFFUSE, RGB15(15, 0, 0));
+	gear(1.0, 4.0, 1.0, 20, 0.7);
+   glPopMatrix(1);
+
+   glPushMatrix();
+   glTranslatef(3.1, -2.0, 0.0);
+   glRotatef(-2.0 * angle - 9.0, 0.0, 0.0, 1.0);
+   glMaterialf(GL_AMBIENT_AND_DIFFUSE, RGB15(0, 15, 0));
+   gear(0.5, 2.0, 2.0, 10, 0.7);
+   glPopMatrix(1);
+
+   glPushMatrix();
+   glTranslatef(-3.1, 4.2, 0.0);
+   glRotatef(-2.0 * angle - 25.0, 0.0, 0.0, 1.0);
+   glMaterialf(GL_AMBIENT_AND_DIFFUSE, RGB15(0, 0, 15));
+   gear(1.3, 2.0, 0.5, 10, 0.7);
+   glPopMatrix(1);
+
+   glPopMatrix(1);
+}
+
+void timerCallBack() {
+	angle += 2;
+   if (angle == 360) angle = 0;
+}
+
+int main() {
+
+	lcdMainOnTop();
+
+	consoleDemoInit();
+
+	videoSetMode(MODE_0_3D);
+
+	glInit();
+	
+	glEnable(GL_ANTIALIAS);
+
+	glClearColor(0,0,0,31);
+	glClearPolyID(63);
+	glClearDepth(0xFFFF);
+	
+	glViewport(0,0,255,191);
+
+	gluLookAt(	0.0, 0.0, 1.0,		//camera possition 
+				0.0, 0.0, 0.0,		//look at
+				0.0, 1.0, 0.0);		//up
+	
+	glLight(0, RGB15(31,31,31) , 0, 0, floattov10(-1.0));
+	
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(60, 256.0 / 192.0, 0.1, 30);
+
+   glMatrixMode(GL_MODELVIEW);
+
+	glTranslatef(0,0,-14);
+
+   glPolyFmt(POLY_ALPHA(31) | POLY_CULL_BACK | POLY_FORMAT_LIGHT0);
+	
+	timerStart(0, ClockDivider_1024, TIMER_FREQ_1024(30), timerCallBack); //add 2 degrees 30 times per second
+   timerStart(1, ClockDivider_1024, 0, timerCallBack); //timer for getting frame rate
+	while (1) {
+
+      timerElapsed(1);
+
+      scanKeys();
+
+      draw(); //probably working while the screen is being drawn. maybe it's more efficient
+
+      swiWaitForVBlank(); 
+
+      consoleClear();
+
+		glFlush(0);
+
+      iprintf("fps : %d", TIMER_SPEED / timerElapsed(1)); //print frame rate
+
+      if (keysHeld() & KEY_START) break;
+
+	}
+
+	return 0;
+}
